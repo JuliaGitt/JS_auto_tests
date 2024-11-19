8717cc44957efc2cb2eafdfc079ed6486887163b
JuliaGitt <julitashevchenko@gmail.com>
2024-11-19 16:38:35 +0100

    adjustedHW

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,27 +1,26 @@
 const { test, expect } = require('@playwright/test');
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
+  await page.goto('/');
 });
 
 test('login', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
 
     //login
-    await page.getByPlaceholder('Username').click();
     await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').click();
     await page.getByPlaceholder('Password').fill('secret_sauce');
-    await page.locator('#login-button').click();
+    await page.getByTestId('login-button').click();
 
-    //verify visibility
-    await expect(page.getByText('Sauce Labs Backpack')).toBeVisible();
-    await page.locator('[data-test="shopping-cart-link"]').click();
-    await expect(page.locator('[data-test="shopping-cart-link"]')).toBeVisible();
-    await page.locator('[data-test="continue-shopping"]').click();
+    //Products (1) title is displayed
+    const products =  page.getByText('Products');
+    await expect(products).toBeVisible();
 
-    //count items on main page >1
-    const listOfitems = page.locator('[data-test="inventory-list"] [data-test="inventory-item"]');
+    //Check that shopping cart is displayed
+    const shoppingCart = page.getByTestId('shopping-cart-link');
+    await expect(shoppingCart).toBeVisible();
+
+    //More than 1 product (3) is displayed
+    const listOfitems = page.getByTestId('inventory-list').getByTestId('inventory-item');
     const itemCount = await listOfitems.count();
     expect(itemCount).toBeGreaterThan(1);
 }
